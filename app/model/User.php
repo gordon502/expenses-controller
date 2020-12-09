e32b71afaac75d3b29a42ef4126bf24549de4430
gordon502 <gordonarek@gmail.com>
2020-12-09 12:11:16 +0100

    Add User model

---
+++ app/model/User.php
@@ -0,0 +1,57 @@
+<?php
+
+class User {
+    private $id;
+    private $login;
+    private $email;
+    private $salt;
+    private $pass;
+
+    public function __construct(int $id, string $login, string $email, string $salt, string $pass) {
+        $this->id = $id;
+        $this->login = $login;
+        $this->email = $email;
+        $this->salt = $salt;
+        $this->pass = $pass;
+    }
+
+    public function getId(): int {
+        return $this->id;
+    }
+
+
+    public function getLogin(): string {
+        return $this->login;
+    }
+
+
+    public function getEmail(): string {
+        return $this->email;
+    }
+
+
+    public function setEmail(string $email) {
+        $this->email = $email;
+    }
+
+
+    public function getSalt(): string {
+        return $this->salt;
+    }
+
+
+    public function setSalt(string $salt) {
+        $this->salt = $salt;
+    }
+
+
+    public function getPass(): string {
+        return $this->pass;
+    }
+
+
+    public function setPass(string $pass) {
+        $this->pass = $pass;
+    }
+    
+}
