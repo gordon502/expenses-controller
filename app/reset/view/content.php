3e954528be867729f41606820be1f3bc29c7b896
gordon502 <gordonarek@gmail.com>
2020-12-18 17:14:45 +0100

    Make reset password page AJAX

--- app/reset/view/content.php
+++ app/reset/view/content.php
@@ -25,22 +25,25 @@
             <div class="pure-control-group">
                 <label for="password">New password</label>
                 <input type="password" id="password" name="password" placeholder="New password" />
-                <span class="pure-form-message-inline">Password must be at least 6 characters.</span>
+                <span class="pure-form-message-inline" id="passworderror" style="color: red; visibility: hidden;">Password must have between 6 and 20 characters!</span>
             </div>
             <div class="pure-control-group">
                 <label for="passwordconf">Repeat</label>
                 <input type="password" id="passwordconf" name="passwordconf" placeholder="Repeat above" />
+                <span class="pure-form-message-inline" id="passwordconferror" style="color: red; visibility: hidden">Password and confirmation must be the same</span>
             </div>
             <div class="pure-control-group">
                 <label for="resetcode">Code</label>
                 <input type="text" id="code" name="code" placeholder="Code" />
+                <span class="pure-form-message-inline" id="codeerror" style="color: red; visibility: hidden;">Code has to be 6 signs!</span>
             </div>
             <div class="pure-controls">
                 <button class="pure-button" type="button" id="generateCodeButton" onclick="generateCode()">Generate Code</button>
+                <img src="https://media3.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif" id="spin2" style="visibility: hidden; height: 1em; width: 1em;">
                 <span class="pure-form-message-inline" id="generateCodeMessage"></span>
             </div>
             <div class="pure-controls">
-                <input type="submit" name="reset" value="Change" class="pure-button pure-button-primary">
+                <input type="submit" id="reset" name="reset" value="Change" class="pure-button pure-button-primary">
             </div>
         </div>
     </fieldset>
--- app/reset/view/script.js
+++ app/reset/view/script.js
@@ -3,18 +3,19 @@ function checkUser() {
     console.log(link);
     $.getJSON(link, function(data) {
         $('#spinningGif').css('visibility', 'visible');
-        $('#checkButton').prop('readonly', true);
+        $('#checkButton').prop('disabled', true);
         $('#login').prop('readonly', true);
         $('#email').prop('readonly', true);
         $('#spin').css('visibility', 'visible');
         setTimeout(() => {
             if (data['exists']) {
                 $('#newPasswordSubform').css('visibility', 'visible');
+                $('#error').html('');
                 console.log("eldo");
             }
             else {
                 $('#error').html(data['message']);
-                $('#checkButton').prop('readonly', false);
+                $('#checkButton').prop('disabled', false);
                 $('#login').prop('readonly', false);
                 $('#email').prop('readonly', false);
             }
@@ -27,10 +28,62 @@ function checkUser() {
 function generateCode() {
     const link = `api/generate_reset_code.php?login=${$('#login').val()}&email=${$('#email').val()}`;
     $.getJSON(link, function (data) {
-        $('#generateCodeMessage').html(data['message']);
         if (data['success'])
             $('#generateCodeMessage').css('color', 'green');
         else
             $('#generateCodeMessage').css('color', 'red');
+        $('#spin2').css('visibility', 'visible');
+        $('#generateCodeButton').prop('disabled', true);
+        setTimeout(() => {
+            $('#generateCodeMessage').html(data['message']);
+            $('#generateCodeButton').prop('disabled', false);
+            $('#spin2').css('visibility', 'hidden');
+        }, 2000);
     });
 }
+
+function checkPasswordField() {
+    const pass = $('#password');
+    return 6 <= pass.val().length && pass.val().length <= 20;
+}
+
+function checkPasswordConfField() {
+    const pass = $('#password');
+    const passconf = $('#passwordconf');
+    return pass.val() === passconf.val();
+}
+
+
+function tryEnableChange() {
+    if (checkPasswordField() && checkPasswordConfField() && checkCodeField()) {
+        $('#reset').prop('disabled', false);
+        $('span').css('visibility', 'hidden');
+    }
+    else
+        $('#reset').prop('disabled', true);
+}
+
+function checkInputs(changedInputFunction, selector) {
+    const check = changedInputFunction();
+    if (check) {
+        $(selector).css('visibility', 'hidden');
+    }
+    else {
+        $(selector).css('visibility', 'visible');
+    }
+
+    tryEnableChange();
+}
+
+function checkCodeField() {
+    const code = $('#code');
+    return code.val().length === 6;
+}
+
+$(document).ready(function () {
+    $('#password').change(function (){checkInputs(checkPasswordField, '#passworderror')});
+    $('#passwordconf').change(function (){checkInputs(checkPasswordConfField, '#passwordconferror')});
+    $('#code').change(function (){checkInputs(checkCodeField, '#codeerror')});
+});
+
+
